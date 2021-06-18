338bee29f0cd83bfe86531cd9c91f1d6bc9dea0e
root <root@DESKTOP-HMVUJA1.localdomain>
2021-06-18 20:13:19 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Sources
+[Book Build your own Lisp](http://buildyourownlisp.com/contents)
+[Chomsky hierarchy Wiki](https://en.wikipedia.org/wiki/Chomsky_hierarchy)
--- main.c
+++ main.c
@@ -7,12 +7,12 @@
 static char buffer[2048];
 
 char* readline(char* prompt) {
-		fputs(prompt, stdout);
-		fgets(buffer, 2048, stdin);
-		char* cpy = malloc(strlen(buffer)+1);
-		strcpy(cpy, buffer);
-		cpy[strlen(cpy)-1] = '\0';
-		return cpy;
+	fputs(prompt, stdout);
+	fgets(buffer, 2048, stdin);
+	char* cpy = malloc(strlen(buffer)+1);
+	strcpy(cpy, buffer);
+	cpy[strlen(cpy)-1] = '\0';
+	return cpy;
 }
 
 void add_history(char* unused) {}
@@ -24,13 +24,13 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
-		puts("Lisp version 0.1");
-		puts("Press Ctrl+c to Exit\n");
-		while (1) {
-				char* input = readline("lisp> ");
-				add_history(input);
-				printf("No you're a %s\n", input);
-				free(input);
-		}
-		return 0;
+	puts("JisooLanguage version 0.1");
+	puts("Press Ctrl+c to Exit\n");
+	while (1) {
+		char* input = readline("jisoo> ");
+		add_history(input);
+		printf("No identifier command: %s\n", input);
+		free(input);
+	}
+	return 0;
 }
