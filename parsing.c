e7000821f6a1a6f0c96740078da0073357074579
root <root@DESKTOP-HMVUJA1.localdomain>
2021-06-19 15:39:31 +0200

    update parsing

--- parsing.c
+++ parsing.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "mpc.h"
 
 #ifdef _WIN32
 #include <string.h>
@@ -24,26 +23,37 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
-	mpc_parser_t* Number	= mpc_new("number");
-	mpc_parser_t* Operator	= mpc_new("operator");
-	mpc_parser_t* Expr		= mpc_new("expr");
-	mpc_parser_t* Jisoo		= mpc_new("jisoo");
+
+	mpc_parser_t* Number   = mpc_new("number");
+	mpc_parser_t* Operator = mpc_new("operator");
+	mpc_parser_t* Expr     = mpc_new("expr");
+	mpc_parser_t* Jisoo    = mpc_new("jisoo");
 
 	mpca_lang(MPCA_LANG_DEFAULT,
-	  "                                                     \
-		number   : /-?[0-9]+/ ;                             \
-		operator : '+' | '-' | '*' | '/' ;                  \
-		expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-		jisoo    : /^/ <operator> <expr>+ /$/ ;             \
-	  ",
-	Number, Operator, Expr, Jisoo);
+	"                                                     \
+	number   : /-?[0-9]+/ ;                             \
+	operator : '+' | '-' | '*' | '/' ;                  \
+	expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+	jisoo    : /^/ <operator> <expr>+ /$/ ;             \
+	", Number, Operator, Expr, Jisoo);
 	puts("JisooLanguage version 0.1");
 	puts("Press Ctrl+c to Exit\n");
 	while (1) {
+
 		char* input = readline("jisoo> ");
+
+		mpc_result_t r;
+		if(mpc_parse("<stdin>", input, Jisoo, &r)) {
+			mpc_ast_print(r.output);
+			mpc_ast_delete(r.output);
+		} else {
+			mpc_err_print(r.error);
+			mpc_err_delete(r.error);
+		}
+
 		add_history(input);
-		printf("No identifier command: %s\n", input);
 		free(input);
 	}
+	mpc_cleanup(4, Number, Operator, Expr, Jisoo);
 	return 0;
 }
