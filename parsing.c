8ce34792170ecb1ca024e4d6f471719d750e2e06
root <root@DESKTOP-HMVUJA1.localdomain>
2021-06-19 02:36:48 +0200

    add some parser

--- parsing.c
+++ parsing.c
@@ -24,6 +24,19 @@ void add_history(char* unused) {}
 #endif
 
 int main(int argc, char** argv) {
+	mpc_parser_t* Number	= mpc_new("number");
+	mpc_parser_t* Operator	= mpc_new("operator");
+	mpc_parser_t* Expr		= mpc_new("expr");
+	mpc_parser_t* Jisoo		= mpc_new("jisoo");
+
+	mpca_lang(MPCA_LANG_DEFAULT,
+	  "                                                     \
+		number   : /-?[0-9]+/ ;                             \
+		operator : '+' | '-' | '*' | '/' ;                  \
+		expr     : <number> | '(' <operator> <expr>+ ')' ;  \
+		jisoo    : /^/ <operator> <expr>+ /$/ ;             \
+	  ",
+	Number, Operator, Expr, Jisoo);
 	puts("JisooLanguage version 0.1");
 	puts("Press Ctrl+c to Exit\n");
 	while (1) {
