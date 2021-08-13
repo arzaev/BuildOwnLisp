5425b3200da275387e9e3b53a855173050415c1c
arzaev <email@arzaev.com>
2021-08-13 06:27:11 -0400

    add string regex to parsing

--- parsing.c
+++ parsing.c
@@ -1,59 +1,75 @@
 #include "mpc.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
+/* If we are compiling on Windows compile these functions */
 #ifdef _WIN32
 #include <string.h>
 
 static char buffer[2048];
 
+/* Fake readline function */
 char* readline(char* prompt) {
-	fputs(prompt, stdout);
-	fgets(buffer, 2048, stdin);
-	char* cpy = malloc(strlen(buffer)+1);
-	strcpy(cpy, buffer);
-	cpy[strlen(cpy)-1] = '\0';
-	return cpy;
+  fputs(prompt, stdout);
+  fgets(buffer, 2048, stdin);
+  char* cpy = malloc(strlen(buffer)+1);
+  strcpy(cpy, buffer);
+  cpy[strlen(cpy)-1] = '\0';
+  return cpy;
 }
 
+/* Fake add_history function */
 void add_history(char* unused) {}
 
+/* Otherwise include the editline headers */
 #else
-
 #include <editline/readline.h>
 #include <editline/history.h>
 #endif
 
 int main(int argc, char** argv) {
 
-	mpc_parser_t* Number   = mpc_new("number");
-	mpc_parser_t* Operator = mpc_new("operator");
-	mpc_parser_t* Expr     = mpc_new("expr");
-	mpc_parser_t* Jisoo    = mpc_new("jisoo");
+  puts("Jplang Version 0.0.0.0.1");
+  puts("Press Ctrl+c to Exit\n");
 
-	mpca_lang(MPCA_LANG_DEFAULT,
-	"                                                     \
-	number   : /-?[0-9]+/ ;                             \
-	operator : '+' | '-' | '*' | '/' ;                  \
-	expr     : <number> | '(' <operator> <expr>+ ')' ;  \
-	jisoo    : /^/ <operator> <expr>+ /$/ ;             \
-	", Number, Operator, Expr, Jisoo);
-	puts("JisooLanguage version 0.1");
-	puts("Press Ctrl+c to Exit\n");
-	while (1) {
+  /* Create Some Parsers */
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Str   = mpc_new("str");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Jplang    = mpc_new("jplang");
 
-		char* input = readline("jisoo> ");
+  /* Define them with the following Language */
+  mpca_lang(MPCA_LANG_DEFAULT,
+  "                                                     \
+    number   : /-?[0-9]+/ ;                             \
+    str   : /[aA-zZ]+/ ;                             \
+    operator : '+' | '-' | '*' | '/' | '%' ;            \
+    expr     : <number> | '(' <operator> <expr>+ ')' | <str> ;  \
+    jplang    : /^/ <operator> <expr>+ /$/ | <expr>* ;             \
+  ",
+  Number, Str, Operator, Expr, Jplang);
 
-		mpc_result_t r;
-		if(mpc_parse("<stdin>", input, Jisoo, &r)) {
-			mpc_ast_print(r.output);
-			mpc_ast_delete(r.output);
-		} else {
-			mpc_err_print(r.error);
-			mpc_err_delete(r.error);
-		}
 
-		add_history(input);
-		free(input);
+  while (1) {
+
+    /* Now in either case readline will be correctly defined */
+    char* input = readline("jplang> ");
+    add_history(input);
+
+	mpc_result_t r;
+	if (mpc_parse("<stdin>", input, Jplang, &r)) {
+		mpc_ast_print(r.output);
+		mpc_ast_delete(r.output);
+	} else {
+		mpc_err_print(r.error);
+		mpc_err_delete(r.error);
 	}
-	mpc_cleanup(4, Number, Operator, Expr, Jisoo);
-	return 0;
+    free(input);
+
+  }
+
+  mpc_cleanup(4, Number, Operator, Expr, Jplang); 
+  return 0;
 }
